5092d9730615c0ca0e6a95930081a084b051487d
Tatiana Delgadillo <tai.delgadillo@gmail.com>
2026-04-24 20:23:32 -0400

    feat: enhance item cleaning functions and improve Larazon spider parsing logic

--- scrapers/news_scraper/items.py
+++ scrapers/news_scraper/items.py
@@ -4,13 +4,45 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import MapCompose, TakeFirst
+from datetime import datetime
+
+
+def clean_title(title):
+    if title:
+        return title.replace("“", '"').replace("”", '"').strip()
+    return title
+
+
+def clean_body_text(text):
+    # Combinación de tu body_formatter y caracteres especiales
+    if text:
+        cleaned = (
+            text.strip()
+            .replace("\xa0", " ")
+            .replace('"', "")
+            .replace("\ufeff", " ")
+            .replace("“", '"')
+            .replace("”", '"')
+            .replace("\u200b", " ")
+        )
+        return cleaned if cleaned not in [" ", ""] else None
+    return None
+
+
+def format_tags(tag):
+    if tag:
+        return tag.lower().strip()
+    return tag
 
 
 class NewsScraperItem(scrapy.Item):
-    url = scrapy.Field()
-    title = scrapy.Field()
-    body = scrapy.Field()
-    tag = scrapy.Field()
-    section = scrapy.Field()
-    source = scrapy.Field()
-    published_at = scrapy.Field()
+    url = scrapy.Field(
+        input_processor=MapCompose(clean_title), output_processor=TakeFirst()
+    )
+    title = scrapy.Field(output_processor=TakeFirst())
+    body = scrapy.Field(input_processor=MapCompose(clean_body_text))
+    tag = scrapy.Field(input_processor=MapCompose(format_tags))
+    section = scrapy.Field(output_processor=TakeFirst())
+    source = scrapy.Field(output_processor=TakeFirst())
+    published_at = scrapy.Field(output_processor=TakeFirst())
--- scrapers/news_scraper/spiders/larazon.py
+++ scrapers/news_scraper/spiders/larazon.py
@@ -1,10 +1,79 @@
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.linkextractors import LinkExtractor
+from datetime import datetime
+from ..items import NewsScraperItem as NewsItem
 
 
 class LarazonSpider(scrapy.Spider):
     name = "larazon"
-    allowed_domains = ["www.la-razon.com"]
-    start_urls = ["https://www.la-razon.com"]
+    allowed_domains = ["larazon.bo"]
+    start_urls = start_urls = [
+        f"https://larazon.bo/tags/feminicidio/page/2/" for i in range(1, 6)
+    ]
+    deny_section = (
+        [
+            r"/lr-article/",
+            r"/mundo/",
+            r"/voces/",
+            r"/opinion/",
+            r"/la-revista/",
+            r"/politico/",
+            r"/marcas/",
+            r"/economia/",
+        ],
+    )
+
+    def date_formatter(self, url, date_format="%Y%m%d"):
+        try:
+            url_split = url.split("/")
+            date_str = url_split[4] + url_split[5] + url_split[6]
+            date_publish = datetime.strptime(date_str, date_format)
+            return date_publish
+        except Exception as e:
+            self.logger.error(f"Error formatting date: {e}")
+            return None
+
+    def section_formatter(self, url):
+        try:
+            url_split = url.split("/")
+            section = url_split[3]
+            return section
+        except Exception as e:
+            self.logger.error(f"Error formatting section: {e} at URL: {url}")
+            return None
 
     def parse(self, response):
-        pass
+        extractor = LinkExtractor(
+            restrict_css="article.jeg_post.jeg_pl_md_2.format-standard h3.jeg_post_title",
+            deny=self.deny_section,
+        )
+
+        for link in extractor.extract_links(response):
+            yield scrapy.Request(
+                url=link.url,
+                callback=self.parse_article,
+            )
+
+    def parse_article(self, response):
+        loader = ItemLoader(item=NewsItem(), response=response)
+        loader.add_value("url", response.url)
+        loader.add_css("title", "div.entry-header h1::text")
+        loader.add_css("body", "div.content-inner p::text")
+        loader.add_css("tags", "div.jeg_post_tags a::text")
+        loader.add_value("source", self.name)
+        section = self.section_formatter(response.url)
+        if section:
+            loader.add_value("section", section)
+        else:
+            self.logger.warning(f"Not possible to extract section for {response.url}")
+
+        published_at = self.date_formatter(response.url)
+        if published_at:
+            loader.add_value("published_at", published_at)
+        else:
+            self.logger.warning(
+                f"Not possible to extract published_at for {response.url}"
+            )
+
+        yield loader.load_item()
